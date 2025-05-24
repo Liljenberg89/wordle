d1738c063bbd681961e72d66a193190a41a86dde
FilipLiljenberg <filipliljenberg9@.com>
2025-05-24 20:31:09 +0200

    user kan now pick a word

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const playField = () => {
+const getWordle = (event) => {
+  let wordle = [];
+  event.preventDefault();
+  const input = document.querySelector(".input").value;
+  wordle.push(input);
+  const newWordle = wordle[0].split(" ");
+
+  console.log(newWordle);
+
+  playField(newWordle);
+};
+
+const playField = (newWordle) => {
+  console.log(newWordle.length);
   const container = document.querySelector(".container");
+  container.style.setProperty(
+    "grid-template-columns",
+    `repeat(${newWordle.length}, 60px)`
+  );
 
   const x = 6;
-  const y = 5;
 
   for (let i = 0; i < x; i++) {
-    for (let a = 0; a < y; a++) {
+    for (let a = 0; a < newWordle.length; a++) {
       const box = document.createElement("div");
       box.classList.add("box");
       container.appendChild(box);
     }
   }
 };
-
-playField();
