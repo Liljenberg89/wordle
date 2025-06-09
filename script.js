c5627f647edb28f859a82e1284f07c703a8e3591
FilipLiljenberg <filipliljenberg9@.com>
2025-06-09 10:45:25 +0200

    small update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let wordle = [];
+let wordle = ["HEJ"];
 let guess = [];
 let trys = 0;
-
+/*
 const getWordle = (event) => {
   event.preventDefault();
   const input = document.querySelector(".input");
@@ -10,6 +10,7 @@ const getWordle = (event) => {
   document.querySelector("#form").classList.add("hide");
   playField();
 };
+*/
 
 const playField = () => {
   const container = document.querySelector(".container");
@@ -56,22 +57,18 @@ const checkWordle = (guess) => {
 
   for (let i = 0; i < guess.length; i++) {
     if (guess[i] === wordle[0][i]) {
-      const correct = document.querySelector(`[col="${i}"]`);
+      const correct = document.querySelector(`[col="${i}"][row="${trys}"]`);
       correct.style.backgroundColor = "green";
-    } else {
-      console.log("fel");
+    } else if (wordle[0][i].includes(guess[i])) {
+      const contains = document.querySelector(`[col="${i}"][row="${trys}"]`);
+      contains.style.backgroundColor = "orange";
+      console.log("hej");
     }
-    const showGuess = document.querySelector(`[col="${i}"]`);
+
+    const showGuess = document.querySelector(`[col="${i}"][row="${trys}"]`);
     showGuess.innerHTML = guess[i];
   }
-  /*
-  let guess = [];
-  guess.push(userGuess.toLowerCase().split(""));
-  console.log(guess[0]);
-  if (guess[0].length !== wordle[0].length) {
-    console.log("fel");
-  } else {
-    console.log("rätt");
-  }
-    */
+  trys++;
+  guess.length = 0;
 };
+playField();
