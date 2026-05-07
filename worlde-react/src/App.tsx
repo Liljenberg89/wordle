f73981a908150d1db14f931743736692e8ee2a45
Filip Liljenberg <filipliljenberg9@gmail.com>
2026-05-07 17:43:26 +0200

    Keyboard design done

--- worlde-react/src/App.tsx
+++ worlde-react/src/App.tsx
@@ -30,18 +30,35 @@ function App() {
             wordle.map(() => <input className="tile" maxLength={1} />),
           )}
         </div>
+        <KeyBoard />
       </div>
     );
   };
 
-  const keyBoard = () => {
-    const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
-    const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const KeyBoard = () => {
+    const keys1: any = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+    const keys2: any = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["DELETE", "Z", "X", "C", "V", "B", "N", "M", "ENTER"];
 
     return (
       <div>
-        <div></div>
+        <div className="keyboard">
+          <div className="keyboard-rows">
+            {keys1.map((key: any) => (
+              <div className="key">{key}</div>
+            ))}
+          </div>
+          <div className="keyboard-rows">
+            {keys2.map((key: any) => (
+              <div className="key">{key}</div>
+            ))}{" "}
+          </div>
+          <div className="keyboard-rows">
+            {keys3.map((key: any) => (
+              <div className="key">{key}</div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   };
